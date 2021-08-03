93cb70ee92a484e394b168a3ab7ffb3eead44039
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-08-03 13:32:44 -0700

    font poppins added

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,35 @@
 <template>
-  <intro-cards/> 
+    <q-layout container style="height: 100vh" class="shadow-2">
+        <q-header elevated>
+            <q-toolbar class="text-primary">
+                <q-toolbar-title class="text-color">
+                    RedSol
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+            <Intro-cards/> 
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-import introCards from 'src/components/CardsIntro.vue'
+import IntroCards from 'src/components/CardsIntro.vue'
 
 
 export default defineComponent({
     name: 'PageIndex',
     components:{
-        introCards,
+        IntroCards,
     }
 })
 </script>
+
+<style scoped>
+    .text-color{
+        color: white;
+    }
+</style>
