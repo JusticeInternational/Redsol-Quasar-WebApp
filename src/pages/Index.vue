1015148d413fe01318e3281775dd4e0d7f75337c
sandrita <sandramilenagarciaa@gmail.com>
2021-08-03 12:24:34 -0700

    init

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,10 @@
+<!--home-->
 <template>
   <h2>Helooo</h2>
+  <!-- <router-link to = '/login'>login</router-link> -->
+
+  <!-- good practice if we need to change the name in our routes.js file. Here we are only changing the name ones and it will work everywhere else too!-->
+  <router-link :to="{name:'login'}">login</router-link> 
 </template>
 
 <script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,16 @@
 const routes = [
   {
     path: '/',
+    name: 'home',
     component: () => import('pages/Index.vue')
     
   },
-
+  {
+    path: '/login',
+    name:'login', //we always need a name 
+    component: () => import('pages/Login.vue')
+    
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
