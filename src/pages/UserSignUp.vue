e3cf7eba523a99119122bbfd46f5834974051775
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-08-04 14:35:40 -0700

    Added button to user registration form

--- src/pages/UserSignUp.vue
+++ src/pages/UserSignUp.vue
@@ -16,16 +16,16 @@
                                 </q-card-section>
                                 <q-card-section class="row">
                                     <q-form class="q-gutter-md full-width">
-                                        <q-input square filled v-model="firstName" type="text" label="Nombre" />
-                                        <q-input square filled v-model="lastName" type="text" label="Apellido" />
-                                        <q-input square filled v-model="email" type="email" label="Correo" />
+                                        <q-input square filled v-model="user.firstName" type="text" label="Nombre" />
+                                        <q-input square filled v-model="user.lastName" type="text" label="Apellido" />
+                                        <q-input square filled v-model="user.email" type="email" label="Correo" />
                                         
-                                        <q-input v-model="pass" square filled :type="isPwd ? 'password' : 'text'" label="Contrasena">
+                                        <q-input v-model="user.pass" square filled :type="isPassHidden ? 'password' : 'text'" label="Contrasena">
                                             <template v-slot:append>
                                                 <q-icon
-                                                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                                                    :name="isPassHidden ? 'visibility_off' : 'visibility'"
                                                     class="cursor-pointer"
-                                                    @click="isPwd = !isPwd"
+                                                    @click="isPassHidden = !isPassHidden"
                                                 />
                                             </template>
                                         </q-input>
@@ -34,6 +34,12 @@
                                 <q-card-section class="column">
                                     <q-checkbox color="blue" v-model="terms" label="He leido y acepto los Terminos y condiciones" />
                                     <q-checkbox color="blue" v-model="policy" label="Entiendo y aceptop las Politicas de privacidad" />
+                                </q-card-section>
+
+                                <q-card-section class="column">
+                                    <q-btn color="black">
+                                        Continuar
+                                    </q-btn>
                                 </q-card-section>
                             </q-card>
                         </div>
@@ -50,22 +56,22 @@ import {ref} from 'vue'
 
 export default {
     setup(){
-        const   email= ref(''), 
-                firstName= ref(''),
-                lastName= ref(''), 
-                pass= ref('');
-            
+
+        const user = ref({
+            email: '', 
+            firstName: '', 
+            lastName: '', 
+            pass: ''
+        })
+        
 
 
             
         return{
-            email,
-            firstName,
-            lastName,
-            pass, 
+            user,
             policy: ref(false),
             terms: ref(false),
-            isPwd: ref(true)
+            isPassHidden: ref(true)
         }
 
     }
