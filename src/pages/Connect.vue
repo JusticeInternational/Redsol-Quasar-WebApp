2e057e189948f0c25c000e623af9f9fa26c9ffda
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-08-08 11:15:10 -0700

    init

---
+++ src/pages/Connect.vue
@@ -0,0 +1,23 @@
+<template>
+    <q-page class="row justify-center">
+        <Login/>
+        <Reg/>
+    </q-page>
+</template>
+
+<script>
+import Login from 'components/Login.vue'
+import Reg from 'components/SignUpSelector.vue'
+
+export default {
+    components:{
+        Login, 
+        Reg,
+    },
+
+}
+</script>
+
+<style>
+
+</style>
